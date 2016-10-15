f41ef3d20b1218f8253ca6bcf3d9e33c18c2e5b6
Raquel Moss <raquelxmoss@gmail.com>
2016-10-15 17:35:34 +1300

    We have mastered red rectangles

--- HelloWorld.hx
+++ HelloWorld.hx
@@ -1,5 +1,30 @@
-class HelloWorld {
-  static public function main() {
-    trace("Hello World");
+import nme.display.Sprite;
+import nme.events.Event;
+import nme.events.KeyboardEvent;
+import nme.Lib;
+
+class HelloWorld extends Sprite {
+  var rectX : Int = 10;
+
+  public function new() {
+    super();
+    Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
+    addEventListener(Event.ENTER_FRAME, update);
+  }
+
+  private function update(event: Event) {
+    graphics.clear();
+    graphics.beginFill(0xff0000);
+    graphics.drawRect(rectX, 10, 200, 200);
+  }
+
+  private function keyDown(event: KeyboardEvent) {
+    trace(event.charCode);
+
+    switch (event.charCode) {
+      case 32: {
+        rectX += 1;
+      }
+    }
   }
 }
