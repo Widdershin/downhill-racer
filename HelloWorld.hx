ed78e914238cad2c639241d0886b79b9c831ca53
Raquel Moss <raquelxmoss@gmail.com>
2016-10-15 20:49:25 +1300

    Render a sick moving road

--- HelloWorld.hx
+++ HelloWorld.hx
@@ -1,30 +1,84 @@
 import nme.display.Sprite;
+import nme.display.Bitmap;
+import nme.display.BitmapData;
 import nme.events.Event;
 import nme.events.KeyboardEvent;
+import nme.utils.ByteArray;
+import nme.geom.Rectangle;
 import nme.Lib;
+import nme.Memory;
 
 class HelloWorld extends Sprite {
   var rectX : Int = 10;
+  var bitmapData: BitmapData;
+  var rect : Rectangle;
+  var pixels : ByteArray;
+  var stageWidth : Int;
+  var laneMarkingOffsetY : Float = 0;
 
   public function new() {
     super();
-    Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
+
+    var stage = Lib.current.stage;
+    stageWidth = stage.stageWidth;
+
+    Lib.current.addChild(new nme.display.FPS());
+    //stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
     addEventListener(Event.ENTER_FRAME, update);
+    bitmapData = new BitmapData(stage.stageWidth, stage.stageHeight);
+    addChild(new Bitmap(bitmapData));
+
+    rect = bitmapData.rect;
+    var size : Int = bitmapData.width * bitmapData.height * 4;
+
+    pixels = new ByteArray();
+
+    pixels.setLength(size);
+    Memory.select(pixels);
   }
 
   private function update(event: Event) {
-    graphics.clear();
-    graphics.beginFill(0xff0000);
-    graphics.drawRect(rectX, 10, 200, 200);
-  }
+    var stage = Lib.current.stage;
 
-  private function keyDown(event: KeyboardEvent) {
-    trace(event.charCode);
+    var centerX = stageWidth / 2;
+    var roadWidth : Float = 300;
+    var laneMarkingWidth = 8;
+    var laneMarkingHeight = 70;
 
-    switch (event.charCode) {
-      case 32: {
-        rectX += 1;
+    laneMarkingOffsetY += 3;
+
+    for (y in 0...stage.stageHeight) {
+      roadWidth -= 0.1;
+      for (x in 0...stage.stageWidth) {
+        var r = 0;
+        var g = 200;
+        var b = 0;
+
+        if (x > centerX - (roadWidth / 2) && x < centerX + (roadWidth / 2)) {
+          r = 80;
+          g = 80;
+          b = 80;
+        }
+
+        if (x > centerX - (laneMarkingWidth / 2) && x < centerX + (laneMarkingWidth / 2) && (laneMarkingOffsetY + y) % laneMarkingHeight < laneMarkingHeight / 2) {
+          r = 255;
+          g = 255;
+          b = 255;
+        }
+
+                                                        //G R L B
+        Memory.setI32(((stage.stageHeight - y) * stageWidth + x) * 4, rgbaToHex(g, r, 255, b));
       }
     }
+
+    pixels.position = 0;
+    bitmapData.setPixels(rect, pixels);
+
+    pixels.position = 0;
+    bitmapData.setPixels(rect, pixels);
+  }
+
+  private function rgbaToHex(R: Int, G: Int, B: Int, A: Int): Int {
+    return (A & 0xFF) << 24 | (R & 0xFF) << 16 | (G & 0xFF) << 8 | (B & 0xFF);
   }
 }
